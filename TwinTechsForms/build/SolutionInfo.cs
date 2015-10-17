e3dbe461fdfcd3f86ea56b87d1414fcd024761dc
Jin-Wook Chung <jwChung@hotmail.com>
2015-10-18 07:26:55 +0900

    Incremented pre-release version.

--- TwinTechsForms/build/SolutionInfo.cs
+++ TwinTechsForms/build/SolutionInfo.cs
@@ -3,10 +3,10 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-beta02")]
+[assembly: AssemblyInformationalVersion("0.1.0-beta03")]
 
 /*
- * Version 0.1.0-beta02
+ * Version 0.1.0-beta03
  * 
  * - This is the first release.
  */
