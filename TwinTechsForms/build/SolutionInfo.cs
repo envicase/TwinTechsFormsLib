5ae0e1af422de73641be5b54e1c0f7ad7985e5db
Jin-Wook Chung <jwChung@hotmail.com>
2015-10-18 06:31:41 +0900

    Addressed versioning.

--- TwinTechsForms/build/SolutionInfo.cs
+++ TwinTechsForms/build/SolutionInfo.cs
@@ -1,6 +1,12 @@
-﻿
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-beta01")]
+
 /*
- * Version 0.1.0-beta
+ * Version 0.1.0-beta01
  * 
  * - This is the first release.
  */
