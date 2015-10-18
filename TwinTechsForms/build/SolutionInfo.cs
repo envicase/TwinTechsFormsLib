1366873252ef3c7a912baf7efc5d3f2d06391692
Jin-Wook Chung <jwChung@hotmail.com>
2015-10-18 14:26:55 +0900

    Incremented minor version,
    to indicate that this is the first release.

--- TwinTechsForms/build/SolutionInfo.cs
+++ TwinTechsForms/build/SolutionInfo.cs
@@ -3,10 +3,10 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-beta03")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
 
 /*
- * Version 0.1.0-beta03
+ * Version 0.1.0
  * 
  * - This is the first release.
  */
