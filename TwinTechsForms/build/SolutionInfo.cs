fa1d8379c18459c7edf54781304519abb69d0483
Jin-Wook Chung <jwChung@hotmail.com>
2015-10-19 00:29:49 +0900

    Incremented patch version.

--- TwinTechsForms/build/SolutionInfo.cs
+++ TwinTechsForms/build/SolutionInfo.cs
@@ -3,10 +3,10 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.1")]
-[assembly: AssemblyInformationalVersion("0.1.1-pre01")]
+[assembly: AssemblyInformationalVersion("0.1.1")]
 
 /*
- * Version 0.1.0-pre01
+ * Version 0.1.1
  * 
- * - To test that the sources is available on the symbol sever.
+ * - Renamed XLabs to TwinTechs to resolve conflict namespace.
  */
