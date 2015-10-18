3766a8340f85fad2ebd7ad082bb56cc8ee0a4867
Jin-Wook Chung <jwChung@hotmail.com>
2015-10-19 06:36:21 +0900

    Incremented pre-release version.

--- TwinTechsForms/build/SolutionInfo.cs
+++ TwinTechsForms/build/SolutionInfo.cs
@@ -3,10 +3,10 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.2")]
-[assembly: AssemblyInformationalVersion("0.1.2-test05")]
+[assembly: AssemblyInformationalVersion("0.1.2-test06")]
 
 /*
- * Version 0.1.2-test05
+ * Version 0.1.2-test06
  * 
  * - To test the SDWebImageOptions options.
  */
